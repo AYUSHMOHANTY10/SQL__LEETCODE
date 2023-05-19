6e94370afea2346be5b20b436f79c3e654670636
AYUSH MOHANTY <92939434+AYUSHMOHANTY10@users.noreply.github.com>
2023-05-19 23:00:01 +0530

    Update Sol.sql

--- 8) 1581. Customer Who Visited but Did Not Make Any Transactions/Sol.sql
+++ 8) 1581. Customer Who Visited but Did Not Make Any Transactions/Sol.sql
@@ -1,1 +1,1 @@
-SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans from Visits v LEFT JOIN Transactions t ON v.visit_id = t.visit_id  WHERE t.transaction_id IS NULL GROUP BY v.customer_id; 
+SELECT customer_id, COUNT(visit_id) as count_no_trans FROM Visits WHERE visit_id NOT IN (SELECT visit_id FROM Transactions) GROUP BY customer_id;
