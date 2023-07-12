e4abe1015615916a6803b633926f76879f2df4c0
AYUSH MOHANTY <92939434+AYUSHMOHANTY10@users.noreply.github.com>
2023-07-12 11:34:00 +0530

    Create Sol.sql

---
+++ 18) 1633 Percentage of Users Attended a Contest/Sol.sql
@@ -0,0 +1,2 @@
+SELECT r.contest_id, round(count(r.contest_id)*100/(select count(user_id) from users),2) as percentage from users u 
+join register r on u.user_id=r.user_id group by r.contest_id order by percentage desc,r.contest_id asc; 
